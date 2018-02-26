90331521a04f91a7a5dcedb55244de214b1942da
Shuvalov Anton <anton_shuvalov@me.com>
2018-02-27 00:13:20 +0300

    replace rm with ln -f

--- bin/audio-card-toggler.sh
+++ bin/audio-card-toggler.sh
@@ -6,11 +6,10 @@ DAC_ASOUNDCONF="/home/anton/.asoundrc_oppo_ha2"
 INTERNAL_ASOUNDCONF="/home/anton/.asoundrc_intel_hda"
 DAC_CONNECTED=`lsusb | grep "$DAC_ID"`
 
-rm $ASOUNDRC
 if lsusb | grep ${DAC_ID}; then
-  ln -s $DAC_ASOUNDCONF $ASOUNDRC
+  ln -sf $DAC_ASOUNDCONF $ASOUNDRC
 else
-  ln -s $INTERNAL_ASOUNDCONF $ASOUNDRC
+  ln -sf $INTERNAL_ASOUNDCONF $ASOUNDRC
 fi
 
 
