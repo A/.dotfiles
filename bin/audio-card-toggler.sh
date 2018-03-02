727d5b0f8838e59cdce795a5e6d25aa5a8f8187e
Shuvalov Anton <anton_shuvalov@me.com>
2018-03-02 20:57:21 +0300

    add alsa configs to dotfiles + update path in toggler

--- bin/audio-card-toggler.sh
+++ bin/audio-card-toggler.sh
@@ -2,8 +2,8 @@
 
 DAC_ID="22d9:0426"
 ASOUNDRC="/home/anton/.asoundrc"
-DAC_ASOUNDCONF="/home/anton/.asoundrc_oppo_ha2"
-INTERNAL_ASOUNDCONF="/home/anton/.asoundrc_intel_hda"
+DAC_ASOUNDCONF="/home/anton/.dotfiles/.asoundrc_oppo_ha2"
+INTERNAL_ASOUNDCONF="/home/anton/.dotfiles/.asoundrc_intel_hda"
 DAC_CONNECTED=`lsusb | grep "$DAC_ID"`
 
 if lsusb | grep ${DAC_ID}; then
