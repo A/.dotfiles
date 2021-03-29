36cb66a400401644d4e50b2a7f6a73a0ba1ff64e
Anton Shuvalov <anton@shuvalov.info>
2021-03-30 00:39:45 +0700

    [bin] Add xrandr config script when docked

--- bin/docking.sh
+++ bin/docking.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
-
-DISPLAY_IDS="DP1 DP1-2"
+set -e
 
 MODELINE=$(cvt 3840 2160 30 | grep -i modeline | sed "s/Modeline\ //")
 MODENAME=$(echo $MODELINE | awk -F" " '{print $1}')
 
-xrandr --newmode ${MODELINE}
+xrandr --newmode ${MODELINE} || true
 
-for DISPLAY_ID in $DISPLAY_IDS; do
-  echo "Add mode ${MODENAME} to ${DISPLAY_ID}"
-  xrandr --addmode ${DISPLAY_ID} ${MODENAME}
-  echo "Set mode ${MODENAME} to ${DISPLAY_ID}"
-  xrandr --verbose --output ${DISPLAY_ID} --mode "${MODENAME}"
-done
+xrandr --addmode DP1-1 ${MODENAME}
+xrandr --addmode DP1-2 ${MODENAME}
+
+xrandr --output DP1-1 --mode ${MODENAME} --scale 1.5 --pos 0x0 --primary
+xrandr --output DP1-2 --mode ${MODENAME} --scale 1.5 --pos 5790x0
+
+INTERNAL_MODELINE=$(cvt 2560 1440 60 | grep -i modeline | sed "s/Modeline\ //")
+INTERNAL_MODENAME=$(echo $INTERNAL_MODELINE | awk -F" " '{print $1}')
+
+xrandr --newmode ${INTERNAL_MODELINE} || true
+
+xrandr --verbose --addmode eDP1 ${INTERNAL_MODENAME}
+xrandr --verbose --output eDP1 --mode ${INTERNAL_MODENAME} --scale 1.6 --pos 1690x3240
