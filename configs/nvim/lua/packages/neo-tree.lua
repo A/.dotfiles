df4ed8aa6a6722ff6b37473b5c7689d5489cab2d
Anton Shuvalov <anton@shuvalov.info>
2022-09-04 21:02:15 +0700

    Remove .old

--- configs/nvim/lua/packages/neo-tree.lua
+++ configs/nvim/lua/packages/neo-tree.lua
@@ -24,7 +24,6 @@ local function install(use)
 end
 
 local function setup()
-    -- Unless you are still migrating, remove the deprecated commands from v1.x
   vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 
   -- If you want icons for diagnostic errors, you'll need to define them somewhere:
